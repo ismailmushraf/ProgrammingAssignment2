49d92dadd8dac8d979afc112fc3e84a34728501b
Ismail Musharaf <ismailmushraf@gmail.com>
2019-09-16 11:30:16 +0530

    updated the comments.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,14 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
+  s <- NULL  #s is the variable to store the solved matrix
+  set <- function(y) { # the function to set the matrix
+    x <<- y  
     s <<- NULL
   }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
+  get <- function() x # it return the matrix
+  setsolve <- function(solve) s <<- solve # it used to set the solved matrix to variable s
+  getsolve <- function() s # used to access the solved matrix
   list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
@@ -20,13 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  s <- x$getsolve()
-  if(!is.null(s)){
+  s <- x$getsolve() #getting the solved matrix if there is already calculated(cache value)
+  if(!is.null(s)){ #checking there is a solved matrix or not
     print("getting cached data")
     return(s)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+  data <- x$get() #assigning the matrix to data variable
+  s <- solve(data, ...) #assigning the solved matrix of data matrix to variable s
+  x$setsolve(s) # setting the solved matrix to makecacheMatrix object
+  s #return solved matrix value
 }
